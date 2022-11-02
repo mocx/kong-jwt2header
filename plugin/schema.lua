a88d70f97d0e355b46f1a3cefdc2e43857365f2d
Mooketsi Regoeng <mooketsir@bbd.co.za>
2022-11-02 10:13:39 +0200

    config modifications

--- plugin/schema.lua
+++ plugin/schema.lua
@@ -21,37 +21,37 @@ return {
       config = {
         type = "record",
         fields = {
+            {  content_type = { type = "string", default = "application/json", one_of = { "application/json" }, }, },
+            {  method = { type = "string", default = "POST", one_of = { "POST", "PUT", "PATCH" }, }, },
+            {  timeout = { type = "number", default = 10000 }, },
+            {  keepalive = { type = "number", default = 60000 }, },
+            {  retry_count = { type = "integer", default = 10 }, },
+            {  queue_size = { type = "integer", default = 1 }, },
+            {  flush_timeout = { type = "number", default = 2 }, },
             {  strip_claims = { type     = "string", required = true, default  = "false" }, },
             {  token_required = { type     = "string", required = true, default  = "true" }, },
-            { http_endpoint = typedefs.url({ required = true, encrypted = true }) }, -- encrypted = true is a Kong-Enterprise exclusive feature, does nothing in Kong CE
-            { method = { type = "string", default = "POST", one_of = { "POST", "PUT", "PATCH" }, }, },
-            { content_type = { type = "string", default = "application/json", one_of = { "application/json" }, }, },
-            { timeout = { type = "number", default = 10000 }, },
-            { keepalive = { type = "number", default = 60000 }, },
-            { retry_count = { type = "integer", default = 10 }, },
-            { queue_size = { type = "integer", default = 1 }, },
-            { flush_timeout = { type = "number", default = 2 }, },
-            { headers = {
-              type = "map",
-              keys = typedefs.header_name {
-                match_none = {
-                  {
-                    pattern = "^[Hh][Oo][Ss][Tt]$",
-                    err = "cannot contain 'Host' header",
-                  },
-                  {
-                    pattern = "^[Cc][Oo][Nn][Tt][Ee][Nn][Tt]%-[Ll][Ee][nn][Gg][Tt][Hh]$",
-                    err = "cannot contain 'Content-Length' header",
-                  },
-                  {
-                    pattern = "^[Cc][Oo][Nn][Tt][Ee][Nn][Tt]%-[Tt][Yy][Pp][Ee]$",
-                    err = "cannot contain 'Content-Type' header",
+            {  http_endpoint = typedefs.url({ required = true, encrypted = true }) }, -- encrypted = true is a Kong-Enterprise exclusive feature, does nothing in Kong CE
+            {  headers = {
+                type = "map",
+                keys = typedefs.header_name {
+                  match_none = {
+                    {
+                      pattern = "^[Hh][Oo][Ss][Tt]$",
+                      err = "cannot contain 'Host' header",
+                    },
+                    {
+                      pattern = "^[Cc][Oo][Nn][Tt][Ee][Nn][Tt]%-[Ll][Ee][nn][Gg][Tt][Hh]$",
+                      err = "cannot contain 'Content-Length' header",
+                    },
+                    {
+                      pattern = "^[Cc][Oo][Nn][Tt][Ee][Nn][Tt]%-[Tt][Yy][Pp][Ee]$",
+                      err = "cannot contain 'Content-Type' header",
+                    },
                   },
                 },
-              },
-              values = {
-                type = "string",
-              },
+                values = {
+                  type = "string",
+                },
             }},
             { custom_fields_by_lua = typedefs.lua_code },          
         },
