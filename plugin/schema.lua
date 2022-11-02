74b21b9fdbe24d1d331f48b64bd6c1acf77000f4
Mooketsi Regoeng <mooketsir@bbd.co.za>
2022-11-02 09:59:04 +0200

    added custom valdator

--- plugin/schema.lua
+++ plugin/schema.lua
@@ -1,4 +1,5 @@
 local typedefs = require "kong.db.schema.typedefs"
+local url = require "socket.url"
 
 
 return {
@@ -14,7 +15,7 @@ return {
       consumer = typedefs.no_consumer,
     },
     {
-      protocols = typedefs.protocols_http,
+      protocols = typedefs.protocols,
     },
     {
       config = {
@@ -23,6 +24,18 @@ return {
             {  strip_claims = { type     = "string", required = true, default  = "false" }, },
             {  token_required = { type     = "string", required = true, default  = "true" }, },          
         },
+        custom_validator = function(config)
+          -- check no double userinfo + authorization header
+          local parsed_url = url.parse(config.http_endpoint)
+          if parsed_url.userinfo and config.headers and config.headers ~= ngx.null then
+            for hname, hvalue in pairs(config.headers) do
+              if hname:lower() == "authorization" then
+                return false, "specifying both an 'Authorization' header and user info in 'http_endpoint' is not allowed"
+              end
+            end
+          end
+          return true
+        end,
       },
     },
   },
