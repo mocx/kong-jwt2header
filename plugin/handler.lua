bc4fd9a07fd5d10cf456af135a7bce467d9d3486
Mooketsi Regoeng <mooketsir@bbd.co.za>
2022-11-02 10:26:50 +0200

    added methods

--- plugin/handler.lua
+++ plugin/handler.lua
@@ -104,5 +104,109 @@ function JWT2Header:access(conf)
 end
 
 
+-- Parse host url.
+-- @param `url` host url
+-- @return `parsed_url` a table with host details:
+-- scheme, host, port, path, query, userinfo
+local function parse_url(host_url)
+  local parsed_url = parsed_urls_cache[host_url]
+
+  if parsed_url then
+    return parsed_url
+  end
+
+  parsed_url = url.parse(host_url)
+  if not parsed_url.port then
+    if parsed_url.scheme == "http" then
+      parsed_url.port = 80
+    elseif parsed_url.scheme == "https" then
+      parsed_url.port = 443
+    end
+  end
+  if not parsed_url.path then
+    parsed_url.path = "/"
+  end
+
+  parsed_urls_cache[host_url] = parsed_url
+
+  return parsed_url
+end
+
+-- Sends the provided payload (a string) to the configured plugin host
+-- @return true if everything was sent correctly, falsy if error
+-- @return error message if there was an error
+local function send_payload(self, conf, payload)
+  local method = conf.method
+  local timeout = conf.timeout
+  local keepalive = conf.keepalive
+  local content_type = conf.content_type
+  local http_endpoint = conf.http_endpoint
+
+  local parsed_url = parse_url(http_endpoint)
+  local host = parsed_url.host
+  local port = tonumber(parsed_url.port)
+
+  local httpc = http.new()
+  httpc:set_timeout(timeout)
+
+  table_clear(headers_cache)
+  if conf.headers then
+    for h, v in pairs(conf.headers) do
+      headers_cache[h] = v
+    end
+  end
+
+  headers_cache["Host"] = parsed_url.host
+  headers_cache["Content-Type"] = content_type
+  headers_cache["Content-Length"] = #payload
+  if parsed_url.userinfo then
+    headers_cache["Authorization"] = "Basic " .. encode_base64(parsed_url.userinfo)
+  end
+
+  params_cache.method = method
+  params_cache.body = payload
+  params_cache.keepalive_timeout = keepalive
+
+  local url = fmt("%s://%s:%d%s", parsed_url.scheme, parsed_url.host, parsed_url.port, parsed_url.path)
+
+  -- note: `httpc:request` makes a deep copy of `params_cache`, so it will be
+  -- fine to reuse the table here
+  local res, err = httpc:request_uri(url, params_cache)
+  if not res then
+    return nil, "failed request to " .. host .. ":" .. tostring(port) .. ": " .. err
+  end
+
+  -- always read response body, even if we discard it without using it on success
+  local response_body = res.body
+  local success = res.status < 400
+  local err_msg
+
+  if not success then
+    err_msg = "request to " .. host .. ":" .. tostring(port) ..
+              " returned status code " .. tostring(res.status) .. " and body " ..
+              response_body
+  end
+
+  return success, err_msg
+end
+
+
+local function json_array_concat(entries)
+  return "[" .. concat(entries, ",") .. "]"
+end
+
+
+local function get_queue_id(conf)
+  return fmt("%s:%s:%s:%s:%s:%s",
+             conf.http_endpoint,
+             conf.method,
+             conf.content_type,
+             conf.timeout,
+             conf.keepalive,
+             conf.retry_count,
+             conf.queue_size,
+             conf.flush_timeout)
+end
+
 
 return JWT2Header
