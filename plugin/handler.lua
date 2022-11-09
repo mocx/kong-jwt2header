06da6d30390c1a208793a6ee7a2ff0535ec0c9a4
Mooketsi Regoeng <mooketsir@bbd.co.za>
2022-11-09 17:34:41 +0200

    fix

--- plugin/handler.lua
+++ plugin/handler.lua
@@ -173,22 +173,22 @@ function HttpLogHandler:log(conf)
   --ngx.log(ngx.NOTICE, "HttpLogHandler:log")
   --ngx.log(ngx.NOTICE, "HttpLogHandler:log: incoming body" .. ngx.req.get_body_data())
   local logit = false
-  ngx.log(ngx.NOTICE, "HttpLogHandler:log:upstream: " ..tostring(ngx.var.upstream_uri))
-  ngx.log(ngx.NOTICE, "HttpLogHandler:log:conf_graphurl: " ..conf.graphql_uri)
+  --ngx.log(ngx.NOTICE, "HttpLogHandler:log:upstream: " ..tostring(ngx.var.upstream_uri))
+  --ngx.log(ngx.NOTICE, "HttpLogHandler:log:conf_graphurl: " ..conf.graphql_uri)
   local graph_call =  tostring(ngx.var.upstream_uri) == conf.graphql_uri
-  ngx.log(ngx.NOTICE, "HttpLogHandler:log:isgraph: " ..tostring(graph_call))
-  ngx.log(ngx.NOTICE, "HttpLogHandler:log:httpstatus: " ..tostring(kong.response.get_status()))
+ --ngx.log(ngx.NOTICE, "HttpLogHandler:log:isgraph: " ..tostring(graph_call))
+  --ngx.log(ngx.NOTICE, "HttpLogHandler:log:httpstatus: " ..tostring(kong.response.get_status()))
   if not (graph_call and tostring(kong.response.get_status()):find("2", 1, true) == 1) then
-    ngx.log(ngx.NOTICE, "HttpLogHandler:log:httpstatus: " ..tostring((graph_call and tostring(kong.response.get_status()):find("2", 1, true) == 1)))
+    --ngx.log(ngx.NOTICE, "HttpLogHandler:log:httpstatus: " ..tostring((graph_call and tostring(kong.response.get_status()):find("2", 1, true) == 1)))
     logit = true
   end
   if graph_call then
-    ngx.log(ngx.NOTICE, kong.service.response.get_raw_body())
+    --ngx.log(ngx.NOTICE, kong.service.response.get_raw_body())
     local body_ = cjson.decode(kong.service.response.get_raw_body())
     logit =  body_.errors  ~= nil
   end
 
-  ngx.log(ngx.NOTICE, "HttpLogHandler:log:logit: " ..tostring(logit))
+  --ngx.log(ngx.NOTICE, "HttpLogHandler:log:logit: " ..tostring(logit))
   if logit then
     if conf.custom_fields_by_lua then
       local set_serialize_value = kong.log.set_serialize_value
